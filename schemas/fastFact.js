6c645aeab8d6d8975beabd24d0c151ac196215f4
Jared Brown <jared.brown@elementthree.com>
2020-08-26 21:16:17 -0400

    Add fast fact

---
+++ schemas/fastFact.js
@@ -0,0 +1,12 @@
+export default {
+  title: "Fast Facts",
+  name: "factFact",
+  type: "document",
+  fields: [
+    {
+      title: "Fact",
+      name: "fact",
+      type: "string",
+    },
+  ],
+};
