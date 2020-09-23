c39d8ec7b6696a3101f9b8866805f2a621890b1f
Jared Brown <jared.brown@elementthree.com>
2020-09-23 18:21:23 -0400

    Implement changes ???

---
+++ schemas/plugs/hero.js
@@ -0,0 +1,24 @@
+export default {
+  title: "Hero",
+  name: "hero",
+  type: "object",
+  fields: [
+    {
+      title: "Label",
+      name: "label",
+      description:
+        "Used to label the module in the cms—no effect on the actual module.",
+      type: "string",
+    },
+    {
+      title: "Heading",
+      name: "heading",
+      type: "string",
+    },
+    {
+      title: "Subheading",
+      name: "subheading",
+      type: "string",
+    },
+  ],
+};
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,10 +1,14 @@
 import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import fastFact from "./fastFact";
+import * as plugs from "./plugs";
+
+const allPlugs = Object.values(plugs).map((plugs) => {
+  return { ...plugs, fields: plugs.fields };
+});
 
 export default createSchema({
   name: "default",
 
-  types: schemaTypes.concat([fastFact]),
+  types: schemaTypes.concat([allPlugs]),
 });
